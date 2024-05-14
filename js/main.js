c4ceb71846e56a7c6af7b6cf3c02b87f87831476
Brian Matheo <bmalvarezp@ufpso.edu.co>
2024-05-14 12:24:06 -0500

    fixed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 document.getElementById('link-ropa').addEventListener('click', function() {
+    
     var producto = {
         imagen: document.getElementById('producto-imagen').src,
         nombre: document.getElementById('producto-nombre').textContent,
