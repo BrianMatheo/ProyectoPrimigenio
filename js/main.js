ca779b3c6d163bd8fde7f9258c30da910f706ccf
Brian Matheo <bmalvarezp@ufpso.edu.co>
2024-05-25 23:16:01 -0500

    javascript

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
-/*document.getElementsByClassName('links').addEventListener('click', function(e) {
-    console.log(e.target);
-    var producto = {
-        imagen: document.getElementById('producto-imagen').src,
-        nombre: document.getElementById('producto-nombre').textContent,
-        precio: document.getElementById('producto-precio').textContent,
-        descuento: document.getElementById('producto-descuento').textContent
-    };
-    localStorage.setItem('producto', JSON.stringify(producto));
-});*/
-
-const li = document.getElementsByClassName('links')
- //console.log(li)
 
 
-for(let i = 0; i<li.length; i++) {
-    console.log(li[i])
-}
+
